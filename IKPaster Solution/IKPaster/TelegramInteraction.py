fd2d7f3e14aa0ef0391c9765d22bbdad2e20bbff
Prirogativus <prirogativus@gmail.com>
2025-03-13 17:07:37 +0100

    restructuring tg bot

--- IKPaster Solution/IKPaster/TelegramInteraction.py
+++ IKPaster Solution/IKPaster/TelegramInteraction.py
@@ -1,47 +1,51 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler, Updater
 
 
-token = ""
+token = "8178145378:AAHRbZgao9Iiyz-Wq56CH3__T-bbcySMrtI"
 
-example_device = None
-target_device = None
-is_setup_done = False
-
+FIRST_QUESTION, SECOND_QUESTION = range(2)
+user_answers = {}
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello")
+    user_answers.clear
+    await update.message.reply_text("Hi, give me an example device name:")
+    return FIRST_QUESTION
 
 
 async def example_device_name_set (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Enter example device name:")
+
+    user_answers['example_device'] = update.message.text
+    
+    update.message.reply_text(f"Your example device is{user_answers['example_device']}. Now give me a target device:")
+    
+    return SECOND_QUESTION
+
+    """await update.message.reply_text("Enter example device name:")
     global example_device, target_device
-    example_device = update.message.text
+    example_device = update.message.from_user
     print(f"Example device:{example_device}")
 
     if example_device and target_device != None:
         is_setup_done = True
-        await update.message.reply_text("Setup is done")
+        await update.message.reply_text("Setup is done")"""
     
 
 async def target_device_name_set (update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Enter target device name:")
+
+    user_answers["target_device"] = update.message.text
+    update.message.reply_text(f"Devices are succesfully saved")
+    return ConversationHandler.END
+    """await update.message.reply_text("Enter target device name:")
     global example_device, target_device
-    target_device = update.message.text
+    target_device = update.message.from_user
     print(f"Target device:{target_device}")
 
 
     if example_device and target_device != None:
         is_setup_done = True
-        await update.message.reply_text("Setup is done")
+        await update.message.reply_text("Setup is done")"""
     
-
-def handle_response (text:str) -> str:
-    processed: str = text.lower()
-    if "hello" in processed:
-        return "hi!"
-    print("Starting bot...")
-
 
 
 if __name__ == "__main__":
